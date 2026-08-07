83911cff843087dd48a34371a48c8a111e622751
Henry-ai647 <ofosuhenry30@gmail.com>
2026-08-07 11:46:30 +0000

    Update models.py

--- Backend/models.py
+++ Backend/models.py
@@ -6,7 +6,16 @@ Base = declarative_base()
 class Restaurant(Base):
     __tablename__ = "restaurants"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String)
     phone = Column(String)
     location = Column(String)
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True)
+    customer = Column(String)
+    meal = Column(String)
+    quantity = Column(Integer)
+    status = Column(String)
